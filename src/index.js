d82e2dcc71b8bb06f04a5cc89f15ff956fef2691
DaryaBoychenko <daryam963@yandex.ru>
2024-08-21 17:47:15 +0300

    fix: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(exp) {
+  const pairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '|': '|',
+},      
+open = []
+
+for (let i = 0; i < exp.length; i++)
+    if (pairs[exp[i]]){
+      console.log(exp[i]);
+      open.push(exp[i]);
+      
+    }
+    else if (exp[i] === pairs[open[open.length - 1]]){
+        open.pop();
+    }
+return !open.length
 }
+
